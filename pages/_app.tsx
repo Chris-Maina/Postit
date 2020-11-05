a191d8b89481bc335193024056d22425196e32cf
Chris Maina <mainachrisw@gmail.com>
2020-11-06 01:08:09 +0300

    feat: set up material ui and AuthProvider

---
+++ pages/_app.tsx
@@ -0,0 +1,31 @@
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+// import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { useEffect } from 'react';
+import Head from 'next/head';
+import { AuthProvider } from '../helpers/authHelpers'
+
+
+function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // Remove the server side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  });
+  return (
+    <>
+      <Head>
+        <title>Postit</title>
+      </Head>
+      <AuthProvider>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </AuthProvider>
+    </>
+  )
+}
+
+export default MyApp;
