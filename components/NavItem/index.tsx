bc2433ed37c960239be42ce52cfc73194bc7a9e2
Chris Maina <mainachrisw@gmail.com>
2020-11-16 21:03:42 +0300

    chore: modify NavItem component

--- components/NavItem/index.tsx
+++ components/NavItem/index.tsx
@@ -2,12 +2,14 @@ import Link from 'next/link';
 import ListItem  from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 
+import classes from './NavItem.module.scss';
+
 const NavItem = ({linkText, linkTo}) => {
   return (
     <ListItem button>
       <ListItemText>
         <Link href={linkTo}>
-          <a>{linkText}</a>
+          <a className={classes.Item_Link}>{linkText}</a>
         </Link>
       </ListItemText>
     </ListItem>
